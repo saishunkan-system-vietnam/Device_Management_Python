690ebff03ebf986e7d644a5670c58e7e9290ed88
HungHT <hunght@saisystem.vn>
2019-07-10 16:36:57 +0700

    - update use try - catch

--- src/home/views.py
+++ src/home/views.py
@@ -3,9 +3,13 @@ import logging
 
 # Create your views here.
 def home(request):
-    logging.getLogger("error_logger").error("Hi I am here");
-    logging.getLogger("info_logger").info("Hi I am here info");
-    return render(request, 'home.html')
+    try:
+        #raise Exception("Custom error thrown by newbie developer :D")
+        logging.getLogger("info_logger").info(request);
+        return render(request, 'home.html')
+    except Exception as e:
+        logging.getLogger("error_logger").error(repr(e))
+        pass
 
 def addproduct(request):
     return render(request, 'addproduct.html')
